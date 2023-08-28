8803be6ece7643bd31b75d1b7bd2eb2d8d2e252f
sameeksha2003 <99608275+sameeksha2003@users.noreply.github.com>
2023-08-28 17:08:38 +0530

    Changed the if...elif to match

--- csv_helper.py
+++ csv_helper.py
@@ -6,19 +6,34 @@ def read_csv(file_path):
     data = []
     with open(file_path, 'r', encoding="utf-8") as file:
         reader = csv.reader(file)
+        next(reader)
         for row in reader:
-            if row[1] == "pen":
-                data.append(Pen(row[0], row[1], int(row[2]), int(row[3])))
-            elif row[1] == "pencil":
-                data.append(Pencil(row[0], row[1], int(row[2]), int(row[3])))
-            elif row[1] == "eraser":
-                data.append(Eraser(row[0], row[1], int(row[2]), int(row[3])))
-            elif row[1] == "scale":
-                data.append(Scale(row[0], row[1], int(row[2]), int(row[3])))
-            elif row[1] == "sharpener":
-                data.append(Sharpener(row[0], row[1],
-                            float(row[2]), int(row[3])))
+            prod_id = row[0]
+            prod_name = row[1]
+            prod_price = float(row[2])
+            prod_stock = int(row[3])
+            product_instance = create_product_instance(
+                prod_id, prod_name, prod_price, prod_stock)
+
+            if product_instance:
+                data.append(product_instance)
     return data
+
+
+def create_product_instance(prod_id, prod_name, prod_price, prod_stock):
+    match prod_id:
+        case "P1":
+            return Pen(prod_id, prod_name, prod_price, prod_stock)
+        case "P2":
+            return Pencil(prod_id, prod_name, prod_price, prod_stock)
+        case "P3":
+            return Eraser(prod_id, prod_name, prod_price, prod_stock)
+        case "P4":
+            return Scale(prod_id, prod_name, prod_price, prod_stock)
+        case "P5":
+            return Sharpener(prod_id, prod_name, prod_price, prod_stock)
+        case _:
+            return None
 
 
 def write_csv(file_path, data, fieldnames):
