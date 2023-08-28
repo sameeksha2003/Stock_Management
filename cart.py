876ccb66d2f3f6ba12a2c433b16caaf75f8badee
sameeksha2003 <99608275+sameeksha2003@users.noreply.github.com>
2023-08-28 14:01:29 +0530

    Removed the stock.py functions in cart.py

--- cart.py
+++ cart.py
@@ -1,49 +1,18 @@
-from csv_helper import read_csv, write_csv
-from Model import Pen, Pencil, Eraser, Scale, Sharpener
+from csv_helper import write_csv
+import stock
 
-stock_warehouse = []
 cart = []
-
-
-def load(filename):
-    cart_data = read_csv(filename)
-    for item in cart_data:
-        prod_id = item.pid
-        prod_name = item.pname
-        prod_price = float(item.price)
-        prod_stock = int(item.stock)
-
-        product_instance = create_product_instance(
-            prod_id, prod_name, prod_price, prod_stock)
-
-        if product_instance:
-            stock_warehouse.append(product_instance)
-
-
-def create_product_instance(prod_id, prod_name, prod_price, prod_stock):
-    if prod_id == "P1":
-        return Pen(prod_id, prod_name, prod_price, prod_stock)
-    elif prod_id == "P2":
-        return Pencil(prod_id, prod_name, prod_price, prod_stock)
-    elif prod_id == "P3":
-        return Eraser(prod_id, prod_name, prod_price, prod_stock)
-    elif prod_id == "P4":
-        return Scale(prod_id, prod_name, prod_price, prod_stock)
-    elif prod_id == "P5":
-        return Sharpener(prod_id, prod_name, prod_price, prod_stock)
-    else:
-        return None
 
 
 def list_products():
     print("Available products:")
-    for product in stock_warehouse:
-        print(f"{product.pname}: Rs.{product.price:.2f}")
+    for product in stock.stock_warehouse:
+        print(f"Product: {product.pname}, Price: {product.price}")
 
 
 def purchase(item, quant):
     found = None
-    for product in stock_warehouse:
+    for product in stock.stock_warehouse:
         if product.pname == item:
             found = product
             break
@@ -52,20 +21,16 @@ def purchase(item, quant):
         if found.stock >= quant:
             cart.append({"item": found, "Quantity": quant})
             found.stock -= quant
-            print("Item is added to cart")
+            print(f"Added {quant} {item} to cart.")
         else:
-            print("Not enough stock")
+            print("Not enough stock for", item)
     else:
-        print("Product not found")
+        print("Product not found:", item)
 
 
 def save_cart():
-    fieldnames = ["Item", "Quantity"]
-    stock_data = [
-        {
-            "Item": product["item"].pname,
-            "Quantity": product["Quantity"]
-        }
-        for product in cart
-    ]
-    write_csv('stock.csv', stock_data, fieldnames)
+    fieldnames = ["ProductID", "ProductName", "ProductPrice", "ProductStock"]
+    cart_data = []
+    cart_data = stock.stock_warehouse
+    write_csv('stock.csv',
+              cart_data, fieldnames)
