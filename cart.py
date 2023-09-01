72f2454e75b7ab4624a111fa7256bcd1cfa0f347
sameeksha2003 <99608275+sameeksha2003@users.noreply.github.com>
2023-09-01 12:11:32 +0530

    Update save_cart to show_cart

--- cart.py
+++ cart.py
@@ -6,13 +6,12 @@ cart = []
 def list_products():
     print("Available products:")
     for product in stock.stock_warehouse:
-        print(f"Product: {product.pname}, Price: {product.price}")
+        print(f"Product: {product.pname} at Price: {product.price}")
 
 
 def purchase(item, quant):
     found = next(filter(lambda n: n.pname == item,
-                 stock.stock_warehouse), None)
-
+                 stock.stock_warehouse))
     if found:
         if found.stock >= quant:
             cart.append({"item": found, "Quantity": quant})
@@ -24,5 +23,13 @@ def purchase(item, quant):
         print("Product not found:", item)
 
 
-def save_cart():
-    stock.save()
+def show_cart():
+    total=0
+    for item in cart:
+        product=item["item"]
+        price=product.price
+        quant=item["Quantity"]
+        prod_tot=price*quant
+        print(f"{product.pname} {price:.1f} x {quant} = {prod_tot:.1f}")
+        total+=prod_tot
+    print(f"Total:{total:.1f}")
